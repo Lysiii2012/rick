6c4951e1c2cbd536b3a64a954bf9598fe354ed69
Lysiii2012 <95207405+Lysiii2012@users.noreply.github.com>
2023-03-14 00:34:09 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([{ path: "/", element: <div>Test page</div> }]);
+const router = createBrowserRouter(<App/>);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
   <React.StrictMode>    
